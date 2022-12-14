3d70963b24a1894db4846d384f0a40ecd9719a0a
sreeharshapalliKnoldus <sree.palli@knoldus.com>
2022-12-13 22:04:43 -0600

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to BO panel!
+        </h1>
+      </main>
+    </div>
+  )
+}
