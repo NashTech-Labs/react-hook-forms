f72678599c71e60bfa10259d7f9f8a4d2e704d9c
Pushpendra Sharma <pushpendra.sharma@loblaw.ca>
2023-01-16 17:23:51 +0530

    Log-Out-Top-Header-Added

--- components/Deals/Deals.tsx
+++ components/Deals/Deals.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "@mui/material/Grid";
+import { useAppDispatch, useAppSelector } from "../../store/index";
+import {
+  tokenState,
+  userProfilefn,
+  userRolefn,
+} from "../../store/feature/auth/authSlice";
+import jwt from "jwt-decode";
+import { useGetRolesOfUserMutation } from "../../api/getRoles";
 
 function DealsMainPage() {
+  const userToken = useAppSelector(tokenState);
+  const dispatch = useAppDispatch();
+
+  const [user, setUser] = useState<any>({});
+
+  useEffect(() => {
+    if (userToken !== "") {
+      let value: any = jwt(userToken);
+      dispatch(userProfilefn(value));
+      setUser(value);
+    }
+  }, [userToken, dispatch]);
+
+  const [
+    RolesOfUser,
+    { data: rolesData, isLoading, isError, error: errorMsg },
+  ] = useGetRolesOfUserMutation();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await RolesOfUser(user.email).then((data: any) => {
+        dispatch(userRolefn(data));
+      });
+    };
+
+    if (user?.email) {
+      fetchData();
+    }
+  }, [user, dispatch, RolesOfUser]);
+
   return (
     <Grid
       container
--- store/feature/auth/authSlice.tsx
+++ store/feature/auth/authSlice.tsx
@@ -3,9 +3,13 @@ import { RootState } from "../../index";
 
 export interface AuthState {
   token: string;
+  role: any;
+  userProfile: any;
 }
 const initialState: AuthState = {
   token: "",
+  role: null,
+  userProfile: null,
 };
 
 const authSlice = createSlice({
@@ -15,9 +19,19 @@ const authSlice = createSlice({
     userToken: (state, action) => {
       state.token = action.payload;
     },
+    userRolefn: (state, action) => {
+      state.role = action.payload;
+    },
+    userProfilefn: (state, action) => {
+      state.userProfile = action.payload;
+    },
   },
 });
 
 export const { userToken } = authSlice.actions;
+export const { userRolefn } = authSlice.actions;
+export const { userProfilefn } = authSlice.actions;
 export const tokenState = (state: RootState) => state.user.token;
+export const roleState = (state: RootState) => state.user.role;
+export const userProfileState = (state: RootState) => state.user.userProfile;
 export default authSlice.reducer;
