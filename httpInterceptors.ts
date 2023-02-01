1c54426ccf8e4b860960c56a56bc67f6d119d58b
nks <nirajkumar.sharma@loblaw.ca>
2023-02-01 18:48:14 +0530

    dev-env-config

--- httpInterceptors.ts
+++ httpInterceptors.ts
@@ -1,7 +1,8 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "./store/index";
 
-const baseURL = "http://localhost:8080"
+// const baseURL = "http://localhost:8080"
+const baseURL = "https://cs-bo-panel-bff-dev.loblaw.digital"
 
 export const CustomQuery = () =>
     fetchBaseQuery({
