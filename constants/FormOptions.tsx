e99eda8a5f1b734937faaf5c2bb1b157bd3b57a6
sreeharsha.palli <sreeharsha.palli@loblaw.ca>
2023-02-28 17:48:35 -0500

    payload updates

--- constants/FormOptions.tsx
+++ constants/FormOptions.tsx
@@ -39,3 +39,14 @@ export const dealLevelExclusionOptions = [
     { value: 'no', label: 'No' },
     { value: 'yes', label: 'Yes' }
 ]
+
+export const DEAL_APPLY_TYPE: { [index: string]: string } = {
+    'all': 'ALL',
+    'regular_priced_only': 'REGULAR_ONLY'
+}
+
+export const STACKING_TYPES: { [index: string]: string } = {
+    'override' : 'OVERRIDE',
+    'always_apply': 'ALWAYS_APPLY',
+    'best_apply': 'BEST_APPLIES'
+}
