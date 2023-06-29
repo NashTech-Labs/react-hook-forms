90983be547a60499ab5c6f5a9b45646ee98573b4
sreeharsha.palli <sreeharsha.palli@loblaw.ca>
2023-06-28 21:34:39 -0500

    feat(LDP-17857): add's remove products

--- constants/CreateDealFormStateType.tsx
+++ constants/CreateDealFormStateType.tsx
@@ -1,45 +1,49 @@
 export interface ICreateDealFormState {
-    dealType: string
-    isListValid: boolean
-    title: string
-    description: string
-    identifier: string
-    priority: string
-    stackingType: string
-    dealLevel: string
-    dollarOff: string
-    dealDiscountTab: string
-    percentageOff: string
-    fixedPriceOff: string
-    customPercentageOff: string
-    basketSpend: string
-    basketDiscount: string
-    englishMessage: string
-    frenchMessage: string
-    productsCollectionTab: string
-    productExclusionsCollectionTab: string
-    dealApplyType: string
-    dealLevelOptions: string
-    mch: string[]
-    exmch: string[]
-    liam: string[]
-    exliam: string[]
-    fileMCH: string[]
-    exFileMCH: string[]
-    fileLIAM: string[]
-    exFileLIAM: string[]
-    fileName: string
-    exFileName: string
-    startDatePicker: object | null
-    endDatePicker: object | null
-    startTimePicker: object | null
-    endTimePicker: object | null
-    collectionFileName: string
-    basketDealType: string
-    draftCreatedTimestamp: object | null
-    dealCriteria: any
-    dealCriteriaType: string
-    spendMinimum: string
-    customMinimumSpend: string
-    shippingMethodType: string
+  dealType: string;
+  isListValid: boolean;
+  title: string;
+  description: string;
+  identifier: string;
+  priority: string;
+  stackingType: string;
+  dealLevel: string;
+  dollarOff: string;
+  dealDiscountTab: string;
+  percentageOff: string;
+  fixedPriceOff: string;
+  customPercentageOff: string;
+  basketSpend: string;
+  basketDiscount: string;
+  englishMessage: string;
+  frenchMessage: string;
+  productsCollectionTab: string;
+  productExclusionsCollectionTab: string;
+  dealApplyType: string;
+  dealLevelOptions: string;
+  mch: string[];
+  exmch: string[];
+  liam: string[];
+  exliam: string[];
+  fileMCH: string[];
+  exFileMCH: string[];
+  fileLIAM: string[];
+  exFileLIAM: string[];
+  fileName: string;
+  exFileName: string;
+  startDatePicker: object | null;
+  endDatePicker: object | null;
+  startTimePicker: object | null;
+  endTimePicker: object | null;
+  collectionFileName: string;
+  basketDealType: string;
+  draftCreatedTimestamp: object | null;
+  dealCriteria: any;
+  dealCriteriaType: string;
+  spendMinimum: string;
+  customMinimumSpend: string;
+  shippingMethodType: string;
+  removeMchList: string[];
+  removeLiamList: string[];
+  removeMchExclusionsList: string[];
+  removeLiamExclusionsList: string[];
 }
--- constants/FormOptions.tsx
+++ constants/FormOptions.tsx
@@ -1,100 +1,100 @@
 export const stackTypeOptions: { [index: string]: string } = {
-    'best_apply': 'Best Apply',
-    'always_apply': 'Always Apply',
-    'override': 'Override'
-}
+  best_apply: "Best Apply",
+  always_apply: "Always Apply",
+  override: "Override",
+};
 
 export const stackTypeFreeShipping: { [index: string]: string } = {
-    'always_apply': 'Always Apply'
-}
+  always_apply: "Always Apply",
+};
 
-export const IDENTIFIER_MAX_SIZE = 15
+export const IDENTIFIER_MAX_SIZE = 15;
 
 export const dealApplyOptions: { [index: string]: string } = {
-    'all': 'All',
-    'regular_priced_only': 'Regular priced only'
-}
+  all: "All",
+  regular_priced_only: "Regular priced only",
+};
 
 export const dealLevelOptions = [
-    { value: 'product', label: 'Product' },
-    { value: 'basket', label: 'Basket' }
-]
+  { value: "product", label: "Product" },
+  { value: "basket", label: "Basket" },
+];
 
 export const dealTabs = [
-    { label: 'Dollar ($) off', value: 'dollar' },
-    { label: 'Percentage (%) off', value: 'percentage' },
-    { label: 'Fixed price', value: 'fixed' }
-]
+  { label: "Dollar ($) off", value: "dollar" },
+  { label: "Percentage (%) off", value: "percentage" },
+  { label: "Fixed price", value: "fixed" },
+];
 
 export const percentageOptions = [
-    { value: '10', label: '10%' },
-    { value: '20', label: '20%' },
-    { value: '25', label: '25%' },
-    { value: '40', label: '40%' },
-    { value: 'custom', label: 'Add custom value' }
-]
+  { value: "10", label: "10%" },
+  { value: "20", label: "20%" },
+  { value: "25", label: "25%" },
+  { value: "40", label: "40%" },
+  { value: "custom", label: "Add custom value" },
+];
 
 export const minimumSpendOptions = [
-    { value: 'NO_MINIMUM', label: 'No minimum' },
-    { value: '30', label: '$30' },
-    { value: '50', label: '$50' },
-    { value: '100', label: '$100' },
-    { value: 'CUSTOM', label: 'Add custom value' }
-]
+  { value: "NO_MINIMUM", label: "No minimum" },
+  { value: "30", label: "$30" },
+  { value: "50", label: "$50" },
+  { value: "100", label: "$100" },
+  { value: "CUSTOM", label: "Add custom value" },
+];
 
 export const productCollectionTabs = [
-    { label: "Upload product(s)", value: "uploadProduct" },
-    { label: "Manually add product(s)", value: "addProduct" },
+  { label: "Upload product(s)", value: "uploadProduct" },
+  { label: "Manually add product(s)", value: "addProduct" },
 ];
 
 export const dealLevelExclusionOptions = [
-    { value: 'no', label: 'No' },
-    { value: 'yes', label: 'Yes' }
-]
+  { value: "no", label: "No" },
+  { value: "yes", label: "Yes" },
+];
 
 export const DEAL_APPLY_TYPE: { [index: string]: string } = {
-    'all': 'ALL',
-    'regular_priced_only': 'REGULAR_ONLY'
-}
+  all: "ALL",
+  regular_priced_only: "REGULAR_ONLY",
+};
 
 export const STACKING_TYPES: { [index: string]: string } = {
-    'override': 'OVERRIDE',
-    'always_apply': 'ALWAYS_APPLY',
-    'best_apply': 'BEST_APPLIES'
-}
+  override: "OVERRIDE",
+  always_apply: "ALWAYS_APPLY",
+  best_apply: "BEST_APPLIES",
+};
 
 export const statusOptions: { [index: string]: string } = {
-    'ACTIVE': 'Active',
-    'READY': 'Ready',
-    'DRAFT': 'Draft',
-    'ENDED': 'Ended',
-    'INACTIVE': 'Inactive'
-}
+  ACTIVE: "Active",
+  READY: "Ready",
+  DRAFT: "Draft",
+  ENDED: "Ended",
+  INACTIVE: "Inactive",
+};
 
 export const dealTypeOptions: { [index: string]: string } = {
-    'MULTI_BUY': 'Multi-buy',
-    'DISCOUNT': 'Discount',
-    'FREE_SHIPPING': 'Free shipping',
-}
+  MULTI_BUY: "Multi-buy",
+  DISCOUNT: "Discount",
+  FREE_SHIPPING: "Free shipping",
+};
 
 export const JF_PROMOTION_TYPE: { [index: string]: string } = {
-    'LOYALTY': 'Loyalty',
-    'VOUCHER': 'Voucher'
-}
+  LOYALTY: "Loyalty",
+  VOUCHER: "Voucher",
+};
 
 export const SDM_PROMOTION_TYPE: { [index: string]: string } = {
-    'DEAL': 'Deal',
-    'LOYALTY': 'Loyalty',
-    'VOUCHER': 'Voucher'
-}
+  DEAL: "Deal",
+  LOYALTY: "Loyalty",
+  VOUCHER: "Voucher",
+};
 
-export const LOB_OPTIONS : { [index: string] : string } = {
-    'JOE_FRESH' : 'Joe Fresh',
-    'SHOPPERS_DRUG_MART': 'Shoppers Drug Mart'
-}
+export const LOB_OPTIONS: { [index: string]: string } = {
+  JOE_FRESH: "Joe Fresh",
+  SHOPPERS_DRUG_MART: "Shoppers Drug Mart",
+};
 
-export const DISCOUNT_DEAL_TYPE = "DISCOUNT"
-export const FREE_SHIPPING_DEAL_TYPE = 'FREE_SHIPPING'
-export const MULTI_BUY_DEAL_TYPE = "MULTI_BUY"
+export const DISCOUNT_DEAL_TYPE = "DISCOUNT";
+export const FREE_SHIPPING_DEAL_TYPE = "FREE_SHIPPING";
+export const MULTI_BUY_DEAL_TYPE = "MULTI_BUY";
 
-export const EDIT_SCENARIO_FILED_EXCEPTIONS = ["file", "exFile", "mch", "exmch", "liam", "exliam"]
+export const EDIT_SCENARIO_FILED_EXCEPTIONS = ["file", "exFile"];
