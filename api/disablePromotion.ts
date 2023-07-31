3bab75bb70f10aac57012f7d2ce7159c88073008
Pushpendra Sharma <pushpendra.sharma@loblaw.ca>
2023-07-31 14:15:06 +0530

    Bug/LDP-20182

--- api/disablePromotion.ts
+++ api/disablePromotion.ts
@@ -1,10 +1,15 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { CustomQuery } from "../httpInterceptors";
 
+interface userProfileData {
+    name: string
+}
+
 interface IDisablePromotionsQuery {
     promoType: string
     manageLob: string
     identifier: string
+    userName: userProfileData
 }
 
 interface IDisablePromotionsResult {
@@ -16,12 +21,13 @@ export const disablePromotions = createApi({
     baseQuery: CustomQuery(),
     endpoints: (builder) => ({
         disablePromotion: builder.mutation<IDisablePromotionsResult, IDisablePromotionsQuery>({
-            query: ({ promoType, identifier, manageLob }) => ({
+            query: ({ promoType, identifier, manageLob, userName }) => ({
                 url: `v1/deal/disable?promo_type=${promoType.toLowerCase()}&identifier=${identifier}&manage_lob=${manageLob}`,
                 method: "PATCH",
                 headers: {
                     "X-Loblaw-Tenant-ID": `JOE_FRESH`,
-                    "x-apikey": "6bbd4e6c-ae21-40b3-add3-91240bb07db0"
+                    "x-apikey": "6bbd4e6c-ae21-40b3-add3-91240bb07db0",
+                    'X-User-Name': userName.name
                 },
             }),
         }),
--- components/ManagePromotions/ManagePromotionsModal.tsx
+++ components/ManagePromotions/ManagePromotionsModal.tsx
@@ -12,33 +12,36 @@ import { useDisablePromotionMutation } from '../../api/disablePromotion'
 import { IManagePromotionsFormState } from './ManagePromotions'
 
 interface IManagePromotionsModal {
- closeModal: Function
- open: boolean
- formValues : IManagePromotionsFormState
+    closeModal: Function
+    open: boolean
+    formValues: IManagePromotionsFormState
 }
 
 const modalStyles = {
     content: {
-      width: "27%",
-      top: "40%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      borderRadius: "2px",
-      background: "#fff",
-      boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-      padding: "16px",
-      gap: "10px",
+        width: "27%",
+        top: "40%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+        borderRadius: "2px",
+        background: "#fff",
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+        padding: "16px",
+        gap: "10px",
     },
     overlay: {
-      zIndex: "999",
-      background: "rgba(0,0,0,0.4",
+        zIndex: "999",
+        background: "rgba(0,0,0,0.4",
     },
-  };
+};
 
 const ManagePromotionsModal = ({ closeModal, open, formValues }: IManagePromotionsModal) => {
+
+    const user = useAppSelector(userProfileState);
+
     const [consent, setConsent] = useState(false);
     const [disablePromotion] = useDisablePromotionMutation()
 
@@ -49,24 +52,25 @@ const ManagePromotionsModal = ({ closeModal, open, formValues }: IManagePromotio
     };
 
     const handleSubmit = async () => {
-       const { promotionType, promotionId, levelOfBusiness } = formValues
-       await disablePromotion({
-            promoType : promotionType,
+        const { promotionType, promotionId, levelOfBusiness } = formValues
+        await disablePromotion({
+            promoType: promotionType,
             identifier: promotionId,
-            manageLob: levelOfBusiness
+            manageLob: levelOfBusiness,
+            userName: user
         })
-        .unwrap()
-        .then(() => {
-          closeModal(true)
-          notifySuccess('Deal successfully disabled');
-        })
-        .catch(() => {
-          closeModal(false)
-          notifyError("Oops! Something went wrong", "disable-deal-error");
-        })
-        .finally(() => {
-            setConsent(false)
-        })
+            .unwrap()
+            .then(() => {
+                closeModal(true)
+                notifySuccess('Deal successfully disabled');
+            })
+            .catch(() => {
+                closeModal(false)
+                notifyError("Oops! Something went wrong", "disable-deal-error");
+            })
+            .finally(() => {
+                setConsent(false)
+            })
     }
 
     const handleClose = () => {
@@ -79,59 +83,59 @@ const ManagePromotionsModal = ({ closeModal, open, formValues }: IManagePromotio
         isOpen={open}
         onRequestClose={handleClose}
     >
-    <Box p={1} data-testid="disablePromotionModal" sx={{ padding: 0 }}>
-                <Grid container alignItems="center" my={2}>
-                    <Grid item lg={11}>
-                        <Typography variant="h5" className={classes["modal-heading"]}>
-                            Are you sure?
-                        </Typography>
-                    </Grid>
-                    <Grid item lg={1} sx={{ cursor: "pointer", textAlign: "right" }}>
-                        <CloseIcon onClick={handleClose} data-testid="closeIcon" />
-                    </Grid>
+        <Box p={1} data-testid="disablePromotionModal" sx={{ padding: 0 }}>
+            <Grid container alignItems="center" my={2}>
+                <Grid item lg={11}>
+                    <Typography variant="h5" className={classes["modal-heading"]}>
+                        Are you sure?
+                    </Typography>
                 </Grid>
-                <Typography data-testid="heading" variant="body2" className={classes["info-text"]} mt={4}>
-                  Disabled promotions will no longer be visible to customers if they are active.
-                </Typography>
-                <Box my={3}>
-                    <FormGroup>
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                    checked={consent}
-                                    onChange={handleChange}
-                                    data-testid="consentCheck"
-                                />
-                            }
-                            label="Yes, I understand and would like to proceed."
-                            sx={{ color: "#666B73" }}
-                        />
-                    </FormGroup>
-                </Box>
-                <Grid container mt={10} mb={1}>
-                    <Grid item lg={6}>
-                        <Button
-                            className={classes["exit-btn"]}
-                            onClick={handleClose}
-                            data-testid="exitBtn"
-                        >
-                            Exit
-                        </Button>
-                    </Grid>
-                    <Grid item lg={6} sx={{ textAlign: "right" }}>
-                        <Button
-                            className={classes["submit-btn"]}
-                            variant="contained"
-                            data-testid="submitBtn"
-                            disabled={!consent}
-                            onClick={handleSubmit}
-                        >
-                            Disable
-                        </Button>
-                    </Grid>
+                <Grid item lg={1} sx={{ cursor: "pointer", textAlign: "right" }}>
+                    <CloseIcon onClick={handleClose} data-testid="closeIcon" />
                 </Grid>
+            </Grid>
+            <Typography data-testid="heading" variant="body2" className={classes["info-text"]} mt={4}>
+                Disabled promotions will no longer be visible to customers if they are active.
+            </Typography>
+            <Box my={3}>
+                <FormGroup>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                checked={consent}
+                                onChange={handleChange}
+                                data-testid="consentCheck"
+                            />
+                        }
+                        label="Yes, I understand and would like to proceed."
+                        sx={{ color: "#666B73" }}
+                    />
+                </FormGroup>
             </Box>
- </Modal>           
+            <Grid container mt={10} mb={1}>
+                <Grid item lg={6}>
+                    <Button
+                        className={classes["exit-btn"]}
+                        onClick={handleClose}
+                        data-testid="exitBtn"
+                    >
+                        Exit
+                    </Button>
+                </Grid>
+                <Grid item lg={6} sx={{ textAlign: "right" }}>
+                    <Button
+                        className={classes["submit-btn"]}
+                        variant="contained"
+                        data-testid="submitBtn"
+                        disabled={!consent}
+                        onClick={handleSubmit}
+                    >
+                        Disable
+                    </Button>
+                </Grid>
+            </Grid>
+        </Box>
+    </Modal>
 }
 
 export default ManagePromotionsModal;
