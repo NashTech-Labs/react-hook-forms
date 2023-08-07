901891f321712b256b0083fa40a874f0e583a94d
sreeharshapalliKnoldus <sree.palli@knoldus.com>
2023-08-06 21:36:27 -0500

    feat(LDP-10377): draft changes

--- components/CreateDeal/DraftModal.tsx
+++ components/CreateDeal/DraftModal.tsx
@@ -1,69 +1,93 @@
-import React from 'react'
-import { Button, Typography } from '@mui/material'
-import CircularProgress from '@mui/material/CircularProgress'
-import { useRouter } from 'next/router'
-import styles from './DraftModal.module.css'
-import commonStyles from './Steps.module.css'
-import { useAppDispatch } from '../../store'
-import { updateDraftDeal } from '../../store/feature/deal/draftDealSlice'
-import { updateDealEditing, updateDealStep } from '../../store/feature/deal/dealSlice'
-import CreateDealDefaultFormState from '../../constants/CreateDealDefaultFormState'
-import { updateNewDeal } from '../../store/feature/deal/newDealSlice'
+import React from "react";
+import { Button, Typography } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useRouter } from "next/router";
+import styles from "./DraftModal.module.css";
+import commonStyles from "./Steps.module.css";
+import { useAppDispatch } from "../../store";
+import { updateDraftDeal } from "../../store/feature/deal/draftDealSlice";
+import {
+  updateDealEditing,
+  updateDealStep,
+} from "../../store/feature/deal/dealSlice";
+import CreateDealDefaultFormState from "../../constants/CreateDealDefaultFormState";
+import createVoucherDefaultFormState from "../../constants/CreateVoucherDefaultFormState";
+import { updateNewDeal } from "../../store/feature/deal/newDealSlice";
+import {
+  updateVoucherEditing,
+  updateVoucherType,
+} from "../../store/feature/voucher/voucherSlice";
+import { updateNewVoucher } from "../../store/feature/voucher/newVoucherSlice";
 
 interface IDraftModal {
-  loading: boolean
-  closeModal: Function
+  loading: boolean;
+  closeModal: Function;
+  isVoucher?: boolean;
 }
 
-const DraftModal = ({ loading, closeModal }: IDraftModal) => {
+const DraftModal = ({ loading, closeModal, isVoucher }: IDraftModal) => {
   const router = useRouter();
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
   const handleExit = () => {
-    closeModal()
-    router.push('/deals')
-    dispatch(updateDraftDeal({}))
-    dispatch(updateDealEditing(false))
+    closeModal();
+    if (isVoucher) {
+      router.push("/vouchers");
+      dispatch(updateDraftDeal({}));
+      dispatch(updateVoucherEditing(false));
+      dispatch(updateVoucherType(""));
+      dispatch(updateNewVoucher(createVoucherDefaultFormState));
+      return;
+    }
+    router.push("/deals");
+    dispatch(updateDraftDeal({}));
+    dispatch(updateDealEditing(false));
     dispatch(updateDealStep(""));
-    dispatch(updateNewDeal(CreateDealDefaultFormState))
-  }
+    dispatch(updateNewDeal(CreateDealDefaultFormState));
+  };
 
   const handleClose = () => {
-    closeModal()
-  }
+    closeModal();
+  };
 
-  let content = null
+  let content = null;
   if (loading) {
-    content = <div className={styles['draft-modal-saving']}>
-      <div className={styles['save-text']}>Saving progress...</div>
-      <div>
-        <CircularProgress thickness={5} size={70} />
+    content = (
+      <div className={styles["draft-modal-saving"]}>
+        <div className={styles["save-text"]}>Saving progress...</div>
+        <div>
+          <CircularProgress thickness={5} size={70} />
+        </div>
       </div>
-    </div>
+    );
   } else {
-    content = <div>
-      <Typography className={styles['draft-modal-heading']}>Saved. How would you like to proceed?</Typography>
-      <div className={styles['btn-container']}>
-        <Button
-          data-testid="exit-btn"
-          variant="contained"
-          className={commonStyles.cancelBtn}
-          onClick={handleExit}
-        >
-          Just exit
-        </Button>
-        <Button
-          variant="contained"
-          className={commonStyles.continueBtn}
-          onClick={handleClose}
-        >
-          Continue editing deal
-        </Button>
+    content = (
+      <div>
+        <Typography className={styles["draft-modal-heading"]}>
+          Saved. How would you like to proceed?
+        </Typography>
+        <div className={styles["btn-container"]}>
+          <Button
+            data-testid="exit-btn"
+            variant="contained"
+            className={commonStyles.cancelBtn}
+            onClick={handleExit}
+          >
+            Just exit
+          </Button>
+          <Button
+            variant="contained"
+            className={commonStyles.continueBtn}
+            onClick={handleClose}
+          >
+            Continue editing {isVoucher ? "voucher" : "deal"}
+          </Button>
+        </div>
       </div>
-    </div>
+    );
   }
 
-  return content
-}
+  return content;
+};
 
-export default DraftModal
+export default DraftModal;
