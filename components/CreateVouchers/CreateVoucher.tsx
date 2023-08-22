cad0c424cf3f255dd99a5a83a0a6891a0a65c6cb
sreeharshapalliKnoldus <sree.palli@knoldus.com>
2023-08-21 22:06:31 -0500

    feat(LDP-21060): add's step 1

--- components/CreateVouchers/CreateVoucher.tsx
+++ components/CreateVouchers/CreateVoucher.tsx
@@ -9,108 +9,106 @@ import Button from "@mui/material/Button";
 import commonStyles from "../CreateDeal/Steps.module.css";
 import { useAppDispatch } from "../../store/index";
 import { updateVoucherType } from "../../store/feature/voucher/voucherSlice";
-import ReceiptLongOutlinedIcon from '@mui/icons-material/ReceiptLongOutlined';
+import ReceiptLongOutlinedIcon from "@mui/icons-material/ReceiptLongOutlined";
 import { updateNewVoucher } from "../../store/feature/voucher/newVoucherSlice";
 import createVoucherDefaultFormState from "../../constants/CreateVoucherDefaultFormState";
 
 function CreateVoucher() {
+  const router = useRouter();
 
-    const router = useRouter();
+  const dispatch = useAppDispatch();
 
-    const dispatch = useAppDispatch();
+  const [voucherType, setVoucherType] = useState("");
 
-    const [voucherType, setVoucherType] = useState("")
+  const handleVoucherValue = (type: string) => {
+    setVoucherType(type);
+  };
 
-    const handleVoucherValue = (type: string) => {
-        setVoucherType(type)
-    };
+  const ContinueDeal = () => {
+    dispatch(updateVoucherType(voucherType));
+    dispatch(updateNewVoucher(createVoucherDefaultFormState));
+  };
 
-    const ContinueDeal = () => {
-        dispatch(updateVoucherType(voucherType))
-        dispatch(updateNewVoucher(createVoucherDefaultFormState))
-    };
-
-    return (
-        <Grid mt={5}>
-            <Grid container justifyContent="center">
-                <Grid item lg={6} md={8} sm={9}>
-                    <Grid display="flex" justifyContent="space-between" mt={8}>
-                        <Typography data-testid="createDealTitle" variant="h3" className={commonStyles.heading}>
-                            Create New Voucher
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </Grid>
-
-            <Card className={commonStyles["card-container"]}>
-                <StepLabel currentStep={1} totalSteps={6} />
-                <StepTitle title={"Select voucher type"} />
-
-                <Grid
-                    data-testid="discountdealBtn"
-                    onClick={() => {
-                        handleVoucherValue("promotional");
-                    }}
-                    className={commonStyles["deal-card-container"]}
-                    bgcolor={voucherType === "promotional" ? "#E6ECF6" : "#fff"}
-                >
-                    <DiscountOutlinedIcon className={commonStyles.Discount} />
-                    <Grid className={commonStyles.dealTitle}>
-                        <Typography variant="h6" className={commonStyles.dealType}>
-                            Promotional
-                        </Typography>
-                        <Typography>
-                            Create new promotional voucher
-                        </Typography>
-                    </Grid>
-                </Grid>
-
-                {/* <Grid
-                    data-testid="multidealBtn"
-                    // onClick={() => {
-                    //     handleVoucherValue("serialized");
-                    // }}
-                    className={commonStyles["deal-card-container"]}
-                    bgcolor={voucherType === "serialized" ? "#E6ECF6" : "#fff"}
-                >
-                    <ReceiptLongOutlinedIcon className={commonStyles.Discount} />
-                    <Grid className={commonStyles.dealTitle}>
-                        <Typography variant="h6" className={commonStyles.dealType}>
-                            Serialized
-                        </Typography>
-                        <Typography>
-                            Create new serialized voucher
-                        </Typography>
-                    </Grid>
-                </Grid> */}
-
-            </Card>
-
-            <Grid container justifyContent="center">
-                <Grid item lg={6} md={8} sm={9} mt={5}>
-                    <Grid className={commonStyles.btnSection}>
-                        <Button
-                            data-testid="CancelBtn"
-                            variant="contained"
-                            className={commonStyles.cancelBtn}
-                            onClick={() => router.push("/vouchers")}
-                        >
-                            Cancel
-                        </Button>
-                        <Button
-                            data-testid="ContinueBtn"
-                            onClick={() => ContinueDeal()}
-                            variant="contained"
-                            disabled={!(voucherType === "promotional" || voucherType === "serialized")}
-                            className={commonStyles.continueBtn}
-                        >
-                            Continue
-                        </Button>
-                    </Grid>
-                </Grid>
-            </Grid>
+  return (
+    <Grid mt={5}>
+      <Grid container justifyContent="center">
+        <Grid item lg={6} md={8} sm={9}>
+          <Grid display="flex" justifyContent="space-between" mt={8}>
+            <Typography
+              data-testid="createDealTitle"
+              variant="h3"
+              className={commonStyles.heading}
+            >
+              Create New Voucher
+            </Typography>
+          </Grid>
         </Grid>
-    )
+      </Grid>
+
+      <Card className={commonStyles["card-container"]}>
+        <StepLabel currentStep={1} totalSteps={6} />
+        <StepTitle title={"Select voucher type"} />
+
+        <Grid
+          data-testid="discountdealBtn"
+          onClick={() => {
+            handleVoucherValue("promotional");
+          }}
+          className={commonStyles["deal-card-container"]}
+          bgcolor={voucherType === "promotional" ? "#E6ECF6" : "#fff"}
+        >
+          <DiscountOutlinedIcon className={commonStyles.Discount} />
+          <Grid className={commonStyles.dealTitle}>
+            <Typography variant="h6" className={commonStyles.dealType}>
+              Promotional
+            </Typography>
+            <Typography>Create new promotional voucher</Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          data-testid="multidealBtn"
+          onClick={() => {
+            handleVoucherValue("serialized");
+          }}
+          className={commonStyles["deal-card-container"]}
+          bgcolor={voucherType === "serialized" ? "#E6ECF6" : "#fff"}
+        >
+          <ReceiptLongOutlinedIcon className={commonStyles.Discount} />
+          <Grid className={commonStyles.dealTitle}>
+            <Typography variant="h6" className={commonStyles.dealType}>
+              Serialized
+            </Typography>
+            <Typography>Create new serialized voucher</Typography>
+          </Grid>
+        </Grid>
+      </Card>
+      <Grid container justifyContent="center">
+        <Grid item lg={6} md={8} sm={9} mt={5}>
+          <Grid className={commonStyles.btnSection}>
+            <Button
+              data-testid="CancelBtn"
+              variant="contained"
+              className={commonStyles.cancelBtn}
+              onClick={() => router.push("/vouchers")}
+            >
+              Cancel
+            </Button>
+            <Button
+              data-testid="ContinueBtn"
+              onClick={() => ContinueDeal()}
+              variant="contained"
+              disabled={
+                !(voucherType === "promotional" || voucherType === "serialized")
+              }
+              className={commonStyles.continueBtn}
+            >
+              Continue
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default CreateVoucher
+export default CreateVoucher;
